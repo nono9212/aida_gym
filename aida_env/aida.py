fixed normalization

aida_env/aida.py
@@ -26,7 +26,7 @@ LEGS_TO_MOTOR_NAMES = [
     ['left_rear_calf_hinge', 'left_rear_thigh_hinge', 'left_rear_shoulder_hinge']]
 LIMITS_CALFS = (math.degrees(0.2), math.degrees(2.2))
 LIMITS_THIGH = (math.degrees(-0.8), math.degrees(1.0))
-LIMITS_SHOULDER = (math.degrees(-0.6), math.degrees(0.2))
+LIMITS_SHOULDER = (math.degrees(-0.2), math.degrees(0.6))
 
 LIMITS_MINS = np.array([LIMITS_CALFS[0], LIMITS_THIGH[0], LIMITS_SHOULDER[0]])
 LIMITS_MAX = np.array([LIMITS_CALFS[1], LIMITS_THIGH[1], LIMITS_SHOULDER[1]])
@@ -291,6 +291,7 @@ class Aida:
         upper_bound[2 * self.num_motors:3 * self.num_motors] = 1.0
         upper_bound[3 * self.num_motors:] = 1.0  
         upper_bound[-3 :] = 100.0
+        upper_bound[-1] = 1
         return upper_bound
 
     def GetObservationLowerBound(self):
@@ -301,6 +302,7 @@ class Aida:
         upper_bound[2 * self.num_motors:3 * self.num_motors] = -1.0
         upper_bound[3 * self.num_motors:] = 0  
         upper_bound[-3 :] = -100.0
+        upper_bound[-1] = 0
         return upper_bound
 
     def GetObservationDimension(self):

aida_env/aida_gym_env.py
@@ -159,10 +159,10 @@ class AidaBulletEnv(gym.Env):
     self.reset()
     observation_high = (
         self.aida.GetObservationUpperBound() + OBSERVATION_EPS)
-    observation_high = np.concatenate((observation_high,np.array([100,100])),axis=None)
+    observation_high = np.concatenate((observation_high,np.array([10,10])),axis=None)
     observation_low = (
         self.aida.GetObservationLowerBound() - OBSERVATION_EPS)
-    observation_low = np.concatenate((observation_low,np.array([-100,-100])),axis=None)
+    observation_low = np.concatenate((observation_low,np.array([-10,-10])),axis=None)
     action_dim = NUM_MOTORS
     action_high = np.array([self._action_bound] * action_dim)
     self.action_space = spaces.Box(-action_high, action_high)